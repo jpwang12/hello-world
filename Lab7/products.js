4e0d8e1abc01def6f94f2ea67b4edc4c07842768
Justin Wang <1012jpwang@gmail.com>
2023-10-03 16:52:21 -1000

    lab 7 cont

---
+++ Lab7/products.js
@@ -0,0 +1,120 @@
+name1 = "HTC";
+ price1 = 40.00;
+ image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+ name2 = "Apple";
+ price2 = 75.00;
+ image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+ name3 = "Nokia";
+ price3 = 35.00;
+ image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+ name4 = "Samsung";
+ price4 = 45.00;
+ image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+ name5 = "Blackberry";
+ price5 = 10.00;
+ image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
+console.log("Product variables loaded...");
+
+
+//exercise 1.1
+console.log("exercise 1.1");
+let num_products=5;
+let productCount=1;
+while (productCount <=num_products){
+    console.log(productCount);
+    productCount++;
+}
+
+
+//exercise 1.2
+console.log("exercise 1.2");
+productCount=1;
+while(productCount <= num_products){
+    console.log(productCount+'. '+eval('name'+productCount));
+    productCount++;
+}
+
+
+//exercize 1.3
+console.log("exercise 1.3");
+productCount=1;
+while(productCount <= num_products/2) {
+    console.log(productCount+'. '+eval('name'+productCount));
+    productCount++;
+}
+
+
+//exercise 2.1
+console.log("exercise 2.1");
+productCount=1;
+while (productCount <=num_products){
+    if( productCount > num_products/2){
+        console.log("That's enough");
+        break;
+    }
+    console.log(productCount);
+    productCount++;
+}
+
+
+//exercise 2.2
+console.log("exercise 2.2");
+productCount=1;
+let lowerBound=num_products*.25;
+let upperBound=num_products*.75;
+while(productCount <= num_products) {
+    if(productCount >= lowerBound && productCount<= upperBound){
+        console.log(eval('name'+productCount)+' is sold out!!');
+    } else{
+        console.log(productCount+'. '+eval('name'+productCount));
+    }
+    productCount++;
+}
+
+
+//exercise 2.3
+console.log("exercise 2.3");
+productCount=1;
+while(productCount <= num_products) {
+    if(productCount >= num_products/2){
+        console.log("Don't ask for anything else");
+        //process.exit();
+    }
+    if(productCount >= lowerBound && productCount<= upperBound){
+        console.log(eval('name'+productCount)+' is sold out!!');
+    } else{
+        console.log(productCount+'. '+eval('name'+productCount));
+    }
+    productCount++;
+}
+
+
+//exercise 3.1a
+console.log("exercise 3.1a");
+productCount=1;
+for(let i=1; i<= num_products; i++){
+    if(i >= lowerBound && i<= upperBound){
+        console.log(eval('name'+ i)+' is sold out!!');
+    } else{
+        console.log(i +'. '+eval('name'+ i));
+    }
+    productCount++;
+}
+
+
+//exercise 3.1b
+console.log("exercise 3.1b");
+for(let i=1; eval("typeof name"+i)!='undefined'; i++){
+    if(i >= lowerBound && i<= upperBound){
+        console.log(eval('name'+ i)+' is sold out!!');
+    } else{
+        console.log(i +'. '+eval('name'+ i));
+    }
+    productCount++;
+}
+
